2ae89f4f5353f84eb7dd1cfbe4985569b04de2e9
RicMonster1 <147381654+RicMonster1@users.noreply.github.com>
2024-10-15 23:30:11 -0400

    42 - JavaScript

---
+++ Roadmap/42 - TORNEO DRAGON BALL/javascript/RicJDev.js
@@ -0,0 +1,89 @@
+/*
+  EJERCICIO:
+
+  @RicJDev
+*/
+
+/* PERSONAJES */
+
+class Character {
+  constructor(name, speed, attack, defenseRate) {
+    this.name = name
+
+    this.speed = speed
+    this.attack = attack
+    this.defenseRate = defenseRate
+
+    this.health = 100
+    this.isAlive = true
+  }
+
+  defense(attack) {
+    if (this.defenseRate > attack) {
+      attack -= Math.floor(attack * 0.1)
+
+      console.log(`¡${this.name} ha amortiguado el ataque!`)
+    }
+
+    const dodgeRate = Math.random()
+
+    if (dodgeRate > 0.8) {
+      attack = 0
+
+      console.log(`¡${this.name} ha esquivado el ataque!`)
+    }
+
+    this.health -= attack
+
+    if (this.health < 0) this.health = 0
+    if (this.health === 0) this.isAlive = false
+  }
+}
+
+/* BATALLA */
+
+function displayCharacters(player1, player2) {
+  console.log(`${player1.name}: ${player1.health}`)
+  console.log(`${player2.name}: ${player2.health}`)
+  console.log(' ')
+}
+
+function simulateBattle(player1, player2) {
+  let condition1 = player1.speed > player2.speed
+
+  while (player1.health > 0 && player2.health > 0) {
+    displayCharacters(player1, player2)
+
+    condition1 ? player2.defense(player1.attack) : player1.defense(player2.attack)
+
+    condition1 = !condition1
+  }
+
+  displayCharacters(player1, player2)
+
+  const condition2 = player1.health === 0
+
+  const loser = condition2 ? player1 : player2
+  const winner = !condition2 ? player1 : player2
+
+  console.log(`${loser.name} ha perdido.`)
+
+  return winner
+}
+
+/* TORNEO */
+
+class Tournament {
+  participants = []
+
+  play() {
+    if (this.participants.length % 2 !== 0) {
+      //prettier-ignore
+      console.log('No se puede iniciar un torneo si el numero total de participantes no es potencia de 2')
+
+      return
+    }
+
+    // ...
+  }
+}
