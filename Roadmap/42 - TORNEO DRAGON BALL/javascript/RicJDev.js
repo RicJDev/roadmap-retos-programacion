b733f1f31000585b3aad8b88a24255817c564187
RicMonster1 <147381654+RicMonster1@users.noreply.github.com>
2025-04-27 12:22:56 -0400

    #42 - JavaScript

--- Roadmap/42 - TORNEO DRAGON BALL/javascript/RicJDev.js
+++ Roadmap/42 - TORNEO DRAGON BALL/javascript/RicJDev.js
@@ -4,113 +4,119 @@
   @RicJDev
 */
 
-/* PERSONAJES */
+// PERSONAJES
 
-class Figther {
-  constructor(name, speed, attack, defenseRate) {
+class Fighter {
+  constructor({ name, speed, attackRate, defenseRate }) {
     this.name = name
-
     this.speed = speed
-    this.attack = attack
+    this.attackRate = attackRate
     this.defenseRate = defenseRate
 
     this.health = 100
-    this.isAlive = true
   }
 
-  defense(attack) {
-    if (this.defenseRate > attack) {
-      attack -= Math.floor(attack * 0.1)
+  attack(oponent) {
+    let damage =
+      oponent.defenseRate > this.attackRate ? this.attackRate * 0.1 : this.attackRate - oponent.defenseRate + 1
+    if (Math.random() > 0.8) damage = 0
 
-      console.log(`¡${this.name} ha amortiguado el ataque!`)
-    }
+    oponent.health -= Math.floor(damage)
+    if (oponent.health < 0) oponent.health = 0
 
-    const dodgeRate = Math.random()
-
-    if (dodgeRate > 0.8) {
-      attack = 0
-
-      console.log(`¡${this.name} ha esquivado el ataque!`)
-    }
-
-    this.health -= attack
-
-    if (this.health < 0) this.health = 0
-    if (this.health === 0) this.isAlive = false
+    return damage
   }
 }
 
-/* BATALLA */
-
-function displayFigthers(player1, player2) {
-  console.log(`${player1.name}: ${player1.health}`)
-  console.log(`${player2.name}: ${player2.health}`)
-  console.log(' ')
-}
-
-function simulateBattle(player1, player2) {
-  player1.health = 100
-  player2.health = 100
-
-  let condition1 = player1.speed > player2.speed
-
-  while (player1.health > 0 && player2.health > 0) {
-    displayFigthers(player1, player2)
-
-    condition1 ? player2.defense(player1.attack) : player1.defense(player2.attack)
-
-    condition1 = !condition1
-  }
-
-  displayFigthers(player1, player2)
-
-  const condition2 = player1.health === 0
-
-  const loser = condition2 ? player1 : player2
-  const winner = !condition2 ? player1 : player2
-
-  console.log(`${loser.name} ha perdido.`)
-
-  return winner
-}
-
-/* TORNEO */
+// TORNEO
 
 class Tournament {
-  figthers = []
+  static #fighters = []
 
-  addParticipant(participant) {
-    this.figthers.push(participant)
+  static #shuffle(fighters) {
+    const copy = [...fighters]
+    let currentIndex = copy.length
+
+    while (currentIndex != 0) {
+      let randomIndex = Math.floor(Math.random() * currentIndex)
+      currentIndex--
+      ;[copy[currentIndex], copy[randomIndex]] = [copy[randomIndex], copy[currentIndex]]
+    }
+
+    return copy
   }
 
-  play() {
-    if (this.isPlayable) {
-      console.log('Que inicie el torneo!')
+  static get #rounds() {
+    return Math.log(this.#fighters.length) / Math.log(2)
+  }
 
-      //TODO: logica para gestionar el torneo
+  static add(...fighters) {
+    for (const fighter of fighters) {
+      this.#fighters.push(fighter)
+    }
+  }
 
+  static #battle(fighterA, fighterB) {
+    let first, second
+
+    if (fighterA.speed == fighterB.speed) {
+      ;[first, second] = Math.random() > 0.5 ? [fighterA, fighterB] : [fighterB, fighterA]
+    } else {
+      ;[first, second] = [fighterA, fighterB].sort((a, b) => b.speed - a.speed)
+    }
+
+    while (fighterA.health > 0 && fighterB.health > 0) {
+      first.attack(second)
+      ;[first, second] = [second, first]
+    }
+
+    const winner = fighterA.health > 0 ? fighterA : fighterB
+    winner.health = 100
+
+    console.log(`\n${fighterA.name} vs ${fighterB.name}. ${winner.name} gana!`)
+
+    return winner
+  }
+
+  static start() {
+    console.log(`Cantidad de luchadores actual: ${this.#fighters.length}`)
+
+    if (!Number.isInteger(this.#rounds)) {
+      console.log('Para empezar el torneo la cantidad de luchadores debe ser potencia de 2.')
       return
     }
 
-    //prettier-ignore
-    console.log('No se puede iniciar un torneo si el número total de participantes no es potencia de 2')
-    console.log(`Cantidad actual de participantes: ${this.figthers.length.toLocaleString()}`)
-  }
+    let inTournament = this.#shuffle(this.#fighters)
 
-  get isPlayable() {
-    let count = this.figthers.length
+    let rounds = this.#rounds
+    let roundCounter = 1
 
-    if (count < 1) return false
-    if (count === 1) return true
+    while (rounds > 0) {
+      console.log(`\n${roundCounter}° ronda. `)
+      const winners = []
 
-    while (count % 2 === 0) count = Math.sqrt(count)
+      for (let i = 0; i < inTournament.length; i += 2) {
+        const winner = this.#battle(inTournament[i], inTournament[i + 1])
 
-    return count === Math.sqrt(2)
+        winners.push(winner)
+      }
+
+      inTournament = this.#shuffle(winners)
+      rounds--
+      roundCounter++
+    }
   }
 }
 
-const tournament = new Tournament()
+Tournament.add(
+  new Fighter({ name: 'Goku', speed: 95, attackRate: 90, defenseRate: 85 }),
+  new Fighter({ name: 'Vegeta', speed: 90, attackRate: 88, defenseRate: 80 }),
+  new Fighter({ name: 'Gohan', speed: 85, attackRate: 80, defenseRate: 75 }),
+  new Fighter({ name: 'Freezer', speed: 90, attackRate: 95, defenseRate: 85 }),
+  new Fighter({ name: 'Cell', speed: 88, attackRate: 90, defenseRate: 88 }),
+  new Fighter({ name: 'Majin Buu', speed: 75, attackRate: 70, defenseRate: 80 }),
+  new Fighter({ name: 'Trunks', speed: 85, attackRate: 80, defenseRate: 70 }),
+  new Fighter({ name: 'Android 18', speed: 85, attackRate: 80, defenseRate: 75 })
+)
 
-// tournament.addParticipant(new Figther('Goku', 20, 60, 45))
-
-tournament.play()
+Tournament.start()
